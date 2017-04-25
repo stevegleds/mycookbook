3d90fe5ed43ca29b6c801423b87fed83ee107321
Steve Gledhill <stevegleds@users.noreply.github.com>
2017-04-25 13:37:27 +0100

    25 April 2017 Created simple sqlite database> Next step is to use sqlalchemy following these instructions: http://pythoncentral.io/introductory-tutorial-python-sqlalchemy/

---
+++ files/databases/database.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+
+c = conn.cursor()
+# c.execute('''
+#           CREATE TABLE person
+#           (id INTEGER PRIMARY KEY ASC, name VARCHAR(250) NOT NULL)
+#           ''')
+# c.execute('''
+#           CREATE TABLE address
+#           (id INTEGER PRIMARY KEY ASC, street_name VARCHAR(250), street_number VARCHAR(250),
+#            post_code VARCHAR(250) NOT NULL, person_id INTEGER NOT NULL,
+#            FOREIGN KEY(person_id) REFERENCES person(id))
+#           ''')
+#
+# c.execute('''
+#           INSERT INTO person VALUES(1, 'pythoncentral')
+#           ''')
+# c.execute('''
+#           INSERT INTO address VALUES(1, 'python road', '1', '00000', 1)
+#           ''')
+#
+# conn.commit()
+# conn.close()
+
+conn = sqlite3.connect('example.db')
+
+c = conn.cursor()
+c.execute('SELECT * FROM person')
+print(c.fetchall())
+c.execute('SELECT * FROM address')
+print(c.fetchall())
+conn.close()
